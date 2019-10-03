943be1f1e22cbcebec5e392ec46bf9c029e8bac6
student <student@lab212D1>
2019-10-03 17:54:47 +0200

    iint

---
+++ src/model/UserData.java
@@ -0,0 +1,35 @@
+
+package model;
+
+
+public class UserData {
+    private final String username;
+    private final String password;
+    private final String ip;
+    private final int port;
+
+    public UserData(String username, String password, String ip, int port) {
+        this.username = username;
+        this.password = password;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+    
+    
+}
